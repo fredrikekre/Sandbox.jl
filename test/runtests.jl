9b82aa7f6d600dab18cad5390f960b44099b7656
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-10-08 13:41:51 +0200

    Alternative test-deps implementation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,20 @@
-using Test
-using Sandbox
+# Alternative test process where the package provides a `test/Project.toml`
+# project that Pkg activates and instantiate before running the test script.
+#
+# The code in this file would be internal to Pkg, and packages would provide
+# what's in `tests.jl`.
 
-@test call_fooifier(2.2, 1.1) ≈ 2*2.2^2 - 1.1
-@test call_libfoo(2.2, 1.1) ≈ 2*2.2^2 - 1.1
+append!(empty!(LOAD_PATH), Base.DEFAULT_LOAD_PATH)
+using Pkg
+
+test_project = @__DIR__
+package_project = joinpath(@__DIR__, "..")
+testfile = joinpath(@__DIR__, "tests.jl")
+
+Pkg.activate(test_project)
+Pkg.instantiate()
+Pkg.develop(PackageSpec(path = package_project))
+
+withenv("JULIA_LOAD_PATH" => test_project) do
+    run(`$(Base.julia_cmd()) --startup-file=no $(testfile)`)
+end
---
+++ test/tests.jl
@@ -0,0 +1,5 @@
+using Test
+using Sandbox
+
+@test call_fooifier(2.2, 1.1) ≈ 2*2.2^2 - 1.1
+@test call_libfoo(2.2, 1.1) ≈ 2*2.2^2 - 1.1
